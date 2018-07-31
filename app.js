081f9f75566795a7016b65307aa248062f5ae2da
DESKTOP-39779PL\dg <devingiannoni@gmail.com>
2018-07-31 12:48:05 -0600

    Introduction to JavaScript - Variables 8/8

--- app.js
+++ app.js
@@ -38,3 +38,52 @@ console.log('If it didn\'t become jaundice and went away they could discharge hi
 console.log('But this just being short of jaundice all the time confused them.');
 */
 
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+let notDefined;
+let valueless = undefined;
+console.log(valueless);
+
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+
+molecule += 16;
+// Multiply and assign to particle below
+
+particle *= 6.02;
+// Increment assay by 1
+
+assay++;
+
+let favoriteAnimal = 'bird';
+console.log('My favorite animal: ' + favoriteAnimal)
+
+let myName = 'Devin Giannoni';
+let myCity = 'Cocoa Beach';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+
+
