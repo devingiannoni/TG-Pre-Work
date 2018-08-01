45cafc06528f5e8947f64d5166a443f909c4fc89
DESKTOP-39779PL\dg <devingiannoni@gmail.com>
2018-08-01 10:09:28 -0600

    Learn JavaScript: Arrays 1-3/9

--- app.js
+++ app.js
@@ -347,3 +347,14 @@ const starCount = () => {
   }
 };
 starCount();
+
+
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+console.log(newYearsResolutions);
+
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
