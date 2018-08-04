24d5ab243488efe94236a15668289c24bae7c158
DESKTOP-39779PL\dg <devingiannoni@gmail.com>
2018-08-04 16:53:01 -0600

    Learn JavaScript: Browser Compatibility and Transpilation 1-11/11 - Not much to commit here, this was an excercise mostly in bash, which I enjoyed since I'm familiar with bash.

--- app.js
+++ app.js
@@ -1010,3 +1010,45 @@ console.log(nurseOlynyk.remainingVacationDays);
 nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications);
 
+// Set the variable below to a number
+let esFivePercentageSupport = 95.47;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 87.61;
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", " + " and a sauce made with " + sauce + ".";
+
+{
+  "name": "learning-babel",
+  "version": "1.0.0",
+  "description": "Use Babel to transpile JavaScript ES6 to ES5",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "build": "babel src -d lib"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "babel-cli": "^6.26.0",
+    "babel-preset-env": "^1.7.0"
+  }
+}
+
+"use strict";
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", and a sauce made with " + sauce + ".";
